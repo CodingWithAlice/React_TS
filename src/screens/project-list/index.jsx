8f64b5f39619751aa0ddf483e4b249af5ea23ff0
CodingWithAlice <2269131254@qq.com>
2022-02-01 22:54:30 +0800

    feat:添加两个hook-useMount和useDebounnce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useEffect, useState } from "react"
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 import * as qs from 'qs';
 
 const apiURL = process.env.REACT_APP_URL;
@@ -16,25 +16,24 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   // 负责人也是一个列表
   const [users, setUsers] = useState([]);
+  const debounceParam = useDebounce(param, 2000);
   // 当 param 中的数据发生变化时，页面应该请求接口，按照参数获取列表数据
   useEffect(() => {
-    console.log('qs.stringify(cleanObject(param))=', qs.stringify(cleanObject(param)));
-    console.log('cleanObject(param)=', cleanObject(param));
-    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [param]);
+  }, [debounceParam]);
 
   // 初始化 users - 只触发一次
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiURL}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel param={param} setParam={setParam} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,12 @@
 // 注意 在一个函数里，改变传入的对象本身的做法是不好的
 
+import { useEffect, useState } from "react";
+
 /**
  * 
  */
 export const isFalsy = (value) => value === 0 ? false : !value;
+
 /**
  * 删除对象中值为空的项
  */
@@ -18,3 +21,28 @@ export const cleanObject = (object) => {
   })
   return result
 }
+
+/**
+ * 写一个页面中只执行一次的 custom hook
+ */
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+/**
+ * 写一个防抖 hook debounce
+ */
+export const useDebounce = ((value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout  = setTimeout(() => setDebounceValue(value), delay);
+    // 重点：每次在上一个 useEffect 处理完后再运行
+    return () => clearTimeout(timeout);
+  },[value, delay]);
+
+  return debounceValue;
+})
