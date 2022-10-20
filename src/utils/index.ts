704522c1e4703418ca6d76de595dbca883c29c43
CodingWithAlice <2269131254@qq.com>
2022-10-20 21:19:00 +0800

    feat: 修复页面没有动态展示问题。。。原因我也不知道我当时为什么要这么写

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,73 +3,67 @@
 import { useEffect, useState } from "react";
 
 /**
- * 
+ *
  */
-export const isFalsy = (value: unknown) => value === 0 ? false : !value;
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 /**
  * 删除对象中值为空的项
  */
 export const cleanObject = (object: object) => {
   // 等价于 Object.assign({}, object)
-  const result = {...object};
-  Object.keys(result).forEach(key => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
     // 这里涉及到泛型的概念，暂时略过
     // @ts-ignore
     const value = result[key];
-    if(isFalsy(value)) {
+    if (isFalsy(value)) {
       // @ts-ignore
-      delete result[key]
+      delete result[key];
     }
-  })
-  return result
-}
+  });
+  return result;
+};
 
 /**
  * 写一个页面中只执行一次的 custom hook
  */
 export const useMount = (callback: () => void) => {
   useEffect(() => {
-    callback()
-  }, [])
-}
+    callback();
+  }, []);
+};
 
 /**
  * 写一个防抖 hook debounce
  */
 //  后面用泛型来规范类型
-export const useDebounce = <V>(value: V, delay?: number)=> {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
-    const timeout  = setTimeout(() => setDebounceValue(value), delay);
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
     // 重点：每次在上一个 useEffect 处理完后再运行
     return () => clearTimeout(timeout);
-  },[value, delay]);
+  }, [value, delay]);
 
   return debounceValue;
-}
+};
 
 export const useArray = <T>(persons: T[]) => {
   let [value, setValue] = useState(persons);
   // add
-  function add(item:T) {
-    value.push(item);
-    console.log('add result=', value);
-    setValue(value);
+  function add(item: T) {
+    setValue([...value, item]);
   }
   // remove
-  function removeIndex(index:number) {
-    value.splice(index,1);
-    console.log('remove result=', value);
-    setValue(value);
+  function removeIndex(index: number) {
+    setValue(value.slice(index + 1));
   }
   //clear
   function clear() {
-    value.splice(0,value.length)
-    console.log('clear result=', value);
-    setValue(value);
+    setValue([]);
   }
 
   return {
@@ -77,5 +71,5 @@ export const useArray = <T>(persons: T[]) => {
     clear,
     removeIndex,
     add,
-  }
-}
+  };
+};
