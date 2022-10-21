ca357da5fd1af87928ccebfdd99156969fb60a3e
CodingWithAlice <2269131254@qq.com>
2022-10-21 09:54:23 +0800

    feat: 登陆相关

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import {ProjectListScreen} from 'screens/project-list'
-import {TsReactTest} from 'try-use-array';
+// import {TsReactTest} from 'try-use-array';
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest/>
+      {/* <TsReactTest/> */}
       {/* <ProjectListScreen/> */}
+      <LoginScreen />
     </div>
   );
 }
