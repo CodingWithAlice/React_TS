5970b1146894660151ae1fd7aa0fa4326f674a6b
CodingWithAlice <2269131254@qq.com>
2022-11-13 17:17:39 +0800

    feat: 添加登陆处理

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,37 +1,53 @@
-import { useEffect, useState } from "react"
-export interface Users {
+import { useEffect, useState } from "react";
+export interface User {
   id: string;
   name: string;
   email: string;
   title: string;
   organization: string;
+  token: string;
 }
 interface SearchPanelProps {
   param: {
     name: string;
     personId: string;
-  },
-  setParam: (param: SearchPanelProps['param']) => void;
-  users: Users[];
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+  users: User[];
 }
 // 组件2：表头
-export const SearchPanel = ({param, setParam, users}: SearchPanelProps) => {
-  return <form>
-    <div>
-      {/* 这里 onChange 方法相当于 setParam(Object.assign({}, param, {name: evt.target.name})); */}
-      <input type="text" value={param.name} onChange={evt => setParam({
-        ...param,
-        name: evt.target.value,
-      })} />
-      <select value={param.personId} onChange={evt => setParam({
-        ...param,
-        personId: evt.target.value
-      })}>
-        <option value="">负责人</option>
-        {
-          users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
-        }
-      </select>
-    </div>
-  </form>
-}
+export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
+  return (
+    <form>
+      <div>
+        {/* 这里 onChange 方法相当于 setParam(Object.assign({}, param, {name: evt.target.name})); */}
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value="">负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
